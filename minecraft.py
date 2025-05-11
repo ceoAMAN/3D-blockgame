99b913411e5c9da1cc6b21cdfdc8c2b1e267b121
Aman <161298133+ceoAMAN@users.noreply.github.com>
2025-05-12 01:50:42 +0530

    Update minecraft.py

--- minecraft.py
+++ minecraft.py
@@ -9,9 +9,9 @@ for i in range(20):
  for j in range(20):
   box = Button(color=color.white, model='cube', position=(i, 0, j),
     texture='grass', parent=scene, origin_y=0.5)
-  box.append(box)
+  boxes.append(box)
    
-def inpurt(key):
+def input(key):
   for box in boxes:
    if box.hovered:
      if key == 'left mouse down':
